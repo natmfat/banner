e162b2f876463aef322ba00513617f658d3accb0
natmfat <natmfat@gmail.com>
2023-12-21 00:05:46 -0800

    [🐛] CORS Shenanigans
    https://github.com/vercel/next.js/discussions/47933

--- app/api/banner/route.tsx
+++ app/api/banner/route.tsx
@@ -56,6 +56,11 @@ export const GET = async (req: Request) => {
       height: 400,
       emoji: "twemoji",
       fonts: await fonts,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
     }
   );
 };
